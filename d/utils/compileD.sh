389893977faf3966172e77b344db230c07cdc93a
Dainius <dainius@debianDE.(none)>
2011-03-24 23:51:57 +0200

    Continuing the fight for building this thing under Linux. It appears that it was an x64 build, and I never even noticed! That's really awesome. Though it brings quite a bit of problems, since D is x86-only and x64 version is only in Beta. But hey, if it works, then awesome. Now you need dmd 2.052, which requires GCC 4.4+ and libc6...something. You'll know once you try to install dmd anyway :D

--- d/utils/compileD.sh
+++ d/utils/compileD.sh
@@ -2,7 +2,7 @@
 # Script for rebuilding the D library on Linux.
 # GreatEmerald, 2011
 cd ../src
-dmd -m32 -c -lib ArcomageD.d
-cp ArcomageD.a ../../lib
+dmd -m64 -c -lib ArcomageD.d
+cp ArcomageD.a ../../lib/libArcomageD.a
 echo "Finished rebuilding the D library."
 read
