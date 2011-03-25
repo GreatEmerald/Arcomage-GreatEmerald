f30093688a79c6b681666538b95c56afa0f85983
Dainius <dainius@debianDE.(none)>
2011-03-25 14:41:16 +0200

    D integration in Linux complete. Note that the binaries are actually for AMD64 (I didn't even realise that before). Changed the directory names to reflect that. Now this project has the full power of C, D and Lua!

--- utils/recompile.sh
+++ utils/recompile.sh
@@ -5,10 +5,7 @@
 echo "Attempting to recompile the game, please wait..."
 cd ../src
 make arcomage
-#cp arcomage ../bin/linux
-#make clean
+cp arcomage ../bin/linux-AMD64
+make clean
 echo "Finished compiling. Return to continue."
-read
-dmd -m32 minIni.o arco.o common.o cards.o graphics.o input.o network.o sound.o BFont.o ../lib/ArcomageD.a /usr/lib/libSDL.a /usr/lib/libSDL_image.a /usr/lib/libSDL_net.a /usr/lib/libSDL_mixer.a /usr/lib/liblua5.1.a -of../bin/linux/arcomage 
-echo "Linking complete."
 read
