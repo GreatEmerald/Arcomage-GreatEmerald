8463e9eea4ed826bc9ca999cf116528035dbfe95
GreatEmerald <pastas4@gmail.com>
2011-08-21 18:02:26 +0300

    Created a very simple test CLArcomage program and made it successfully compile and link, but had to use two workarounds because RDMD is buggy at this point. Always on the bleeding edge with D!

---
+++ clarcomage/src/clarcomage.d
@@ -0,0 +1,9 @@
+import std.stdio;
+import arco;
+
+int main()
+{
+	writeln("We didn't crash!");
+    readln();
+    return 0;
+} 
---
+++ clarcomage/src/compile.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+rdmd -ILuaD -Isrc -L-llua --build-only -ofclarcomage clarcomage.d ../../libarcomage/include/libarcomage.a 
