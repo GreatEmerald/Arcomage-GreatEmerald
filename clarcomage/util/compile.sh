0894fea34c308509a7f9e2a60519f1901eac9ead
GreatEmerald <pastas4@gmail.com>
2011-09-06 22:22:39 +0300

    Made the compilation under Linux a little more proper (still needs a better bin place) and made AI understand how to use discard rounds without crashing. It still crashes on certain cards, however.
    Signed-off-by: GreatEmerald <pastas4@gmail.com>

--- clarcomage/util/compile.sh
+++ clarcomage/util/compile.sh
@@ -1,3 +1,4 @@
-#/bin/sh
-rdmd -ILuaD -Isrc -L-llua --build-only -ofclarcomage clarcomage.d ../../libarcomage/include/libarcomage.a 
-#dmd -ILuaD -Isrc -L-llua -ofclarcomage clarcomage.d ../../libarcomage/include/libarcomage.a LuaD/luad/*.d LuaD/luad/conversions/*.d LuaD/luad/c/*.d
+#!/bin/sh
+#rdmd -ILuaD -Isrc -L-llua --build-only -ofclarcomage clarcomage.d ../../libarcomage/include/libarcomage.a 
+cd ../src
+dmd -ILuaD -Isrc -L-llua -ofclarcomage clarcomage.d ../../libarcomage/src/*.d LuaD/luad/*.d LuaD/luad/conversions/*.d LuaD/luad/c/*.d
